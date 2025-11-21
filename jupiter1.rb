04aed96218b882cea7fe21f9773f7ecd65ce8b9d
dharsh2309 <dharsh2309@gmail.com>
2025-11-21 12:33:04 +0530

    changes in jupiter

---
+++ jupiter1.rb
@@ -0,0 +1,5 @@
+tomcat8 //IS//Tomcat8 --DisplayName="Apache Tomcat 8" ^
+     --/Install="C:\Program Files\Tomcat\bin\tomcat8.exe" --Jvm=auto ^
+     --StartMode=jvm --StopMode=jvm ^
+     --StartClass=org.apache.catalina.startup.Bootstrap --StartParams=start ^
+     --StopClass=org.apache.catalina.startup.Bootstrap --StopParams=stop
